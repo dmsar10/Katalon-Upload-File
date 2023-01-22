9cf6a91ae102312dee6fd6b4ede052ed7806871a
Dimas Arya <dimasarya96@gmail.com>
2023-01-22 20:07:10 +0700

    Commit TC02 Upload File (DDT Method)

---
+++ Test Suites/UploadData.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UploadData</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>62066144-4a64-450a-984e-59bccf564641</testSuiteGuid>
+   <testCaseLink>
+      <guid>bef2560c-d132-4ca5-9dc5-53adec9ca02c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02-Uploading file(DDT)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>64c6e8df-2cd5-4138-ae6b-b30d9a27e289</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/UploadData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>64c6e8df-2cd5-4138-ae6b-b30d9a27e289</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Path</value>
+         <variableId>d4707598-4efb-48b5-bd1d-fbc4c9bf9b69</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
